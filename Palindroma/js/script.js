2471ce337115ab1e00e74038e5dd3e0ec8d895bb
Devfrank94 <francescomurro94@gmail.com>
2023-03-28 18:35:08 +0200

    partial commit

---
+++ Palindroma/js/script.js
@@ -0,0 +1,23 @@
+/**Palidroma**
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma*/
+
+/* Quello che sappiamo di una parola palindroma e che se letta in rntrambi i lati da sinistra verso destra e da destra verso sinistra risulterà sempre la stessa Es. otto, anna, radar, kayak.
+
+1. Chiediamo all'utente attraverso un prompt di inserire la parola da controllare.
+
+2. Salviamo la parola in un array ed attraverso il metodo split, dividiamo la parola in lettera per lettera.
+
+3. attraverso il metodo reverse di un array andiamo a riposizionare gli elementi scomposti appunto al contrario.
+
+4. attraverso il metodo join salviamo gli elementi capovolti in una nuova stringa all'interno 
+
+
+
+
+
+
+
+
+*/
+
