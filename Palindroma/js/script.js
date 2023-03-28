c610051ca5788deee77086f50238db778632424e
Devfrank94 <francescomurro94@gmail.com>
2023-03-28 19:12:58 +0200

    partial commit

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -10,14 +10,24 @@ Creare una funzione per capire se la parola inserita è palindroma*/
 
 3. attraverso il metodo reverse di un array andiamo a riposizionare gli elementi scomposti appunto al contrario.
 
-4. attraverso il metodo join salviamo gli elementi capovolti in una nuova stringa all'interno 
+4. attraverso il metodo join salviamo gli elementi capovolti in una nuova stringa all'interno dello stesso array.
 
-
-
-
-
-
-
+5. creamo la condizione che controlli se la parola inserita all'inizio nel prompt corrisponda alla parola gestita nell'array quindi se il controllo soddisfa la condizione restituirà il messaggio che la parola in questione è palindroma, altrimenti no.
 
 */
+let arrWord = [];
 
+const wordIns = prompt('Inserisci una parola da verificare');
+console.log(wordIns);
+
+arrWord.push(wordIns);
+
+console.log(arrWord);
+
+const arrPalin = wordIns.split('');
+
+console.log(arrPalin);
+
+arrPalin.reverse('');
+
+console.log(arrPalin);
